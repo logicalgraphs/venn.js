b9e386dba199453e3f75b10b6c2712762fb6d745
geophf <dauclair@hotmail.com>
2024-06-24 14:45:44 -0400

    showing crypto in forked repository

---
+++ examples/crypto.js
@@ -0,0 +1,13 @@
+var sets = [   {sets: ['KUJI', 'USK'], size: 76112.375},
+   {sets: ['axlUSDC', 'USDC'], size: 28689.857},
+   {sets: ['USDC', 'USK'], size: 103914.14},
+   {sets: ['KUJI', 'ATOM'], size: 28767.488},
+   {sets: ['KUJI', 'USDC'], size: 156620.6},
+   {sets: ['LUNA', 'USK'], size: 80569.29},
+   {sets: ['KUJI'], size: 279081.5},
+   {sets: ['ATOM'], size: 41864.7},
+   {sets: ['USDC'], size: 329450.88},
+   {sets: ['USK'], size: 328721.8},
+   {sets: ['axlUSDC'], size: 66449.51},
+   {sets: ['LUNA'], size: 163669.38}];
+
