228638d2b14404b05f92d1429ca913a769ca3361
geophf <dauclair@hotmail.com>
2024-07-11 17:38:48 -0400

    2024-07-11 Kujira token volumes on FIN

--- examples/crypto.js
+++ examples/crypto.js
@@ -1,13 +1,13 @@
-date = '2024-07-10';
+date = '2024-07-11';
 
-sets = [   {sets: ['KART', 'USK'], size: 37199.85},
-   {sets: ['USDC', 'USK'], size: 56587.57},
-   {sets: ['KUJI', 'USK'], size: 53901.977},
-   {sets: ['KUJI', 'ATOM'], size: 58679.77},
-   {sets: ['KUJI', 'USDC'], size: 124369.44},
-   {sets: ['KUJI'], size: 261568.9},
-   {sets: ['ATOM'], size: 76891.11},
-   {sets: ['USDC'], size: 225989.17},
-   {sets: ['KART'], size: 40077.08},
-   {sets: ['USK'], size: 221238.05}];
+sets = [   {sets: ['KART', 'USK'], size: 34583.133},
+   {sets: ['KUJI', 'ATOM'], size: 23234.2},
+   {sets: ['KUJI', 'USK'], size: 37130.008},
+   {sets: ['KUJI', 'USDC'], size: 61604.88},
+   {sets: ['USDC', 'USK'], size: 58392.13},
+   {sets: ['USK'], size: 174368.03},
+   {sets: ['ATOM'], size: 31703.975},
+   {sets: ['USDC'], size: 157871.4},
+   {sets: ['KART'], size: 36756.867},
+   {sets: ['KUJI'], size: 130530.85}];
 
