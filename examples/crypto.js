7730ea52f4f702852396a69e0bd347e383eeba70
geophf <dauclair@hotmail.com>
2024-07-18 15:39:54 -0400

    pages to display my crypto Venn diagrams

--- examples/crypto.js
+++ examples/crypto.js
@@ -1,13 +1,12 @@
-date = '2024-07-11';
+date = '2024-07-18';
 
-sets = [   {sets: ['KART', 'USK'], size: 34583.133},
-   {sets: ['KUJI', 'ATOM'], size: 23234.2},
-   {sets: ['KUJI', 'USK'], size: 37130.008},
-   {sets: ['KUJI', 'USDC'], size: 61604.88},
-   {sets: ['USDC', 'USK'], size: 58392.13},
-   {sets: ['USK'], size: 174368.03},
-   {sets: ['ATOM'], size: 31703.975},
-   {sets: ['USDC'], size: 157871.4},
-   {sets: ['KART'], size: 36756.867},
-   {sets: ['KUJI'], size: 130530.85}];
+sets = [   {sets: ['KUJI', 'USDC'], size: 153636.66},
+   {sets: ['axlUSDC', 'USK'], size: 35944.36},
+   {sets: ['KUJI', 'USK'], size: 46739.27},
+   {sets: ['axlUSDC', 'USDC'], size: 43882.4},
+   {sets: ['USDC', 'USK'], size: 150522.39},
+   {sets: ['axlUSDC'], size: 84673.09},
+   {sets: ['KUJI'], size: 236328.33},
+   {sets: ['USDC'], size: 372052.3},
+   {sets: ['USK'], size: 305527.47}];
 
